e7ac7782ef4fc2a0db65d32b5962138b3f54cbf4
Zach Briggs <briggszj@gmail.com>
2012-08-02 19:46:05 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Zach::Application.config.secret_token = '4c45a9eaf6bcfe86874004649677b563364b98157b04932d187ddd4f9036aed32466eaffed738cd58fb09c13dd825e1d4963119d1a55dc8324faf69e68c36046'
